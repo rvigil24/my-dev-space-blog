655a01c87c78f16831e805e6810e9d9815f79b1a
Rubén Vigil <ruben.vigil24@gmail.com>
2021-05-30 17:09:21 -0600

    fixed viewport width and added language to document

--- components/Layout.js
+++ components/Layout.js
@@ -10,9 +10,11 @@ export default function Layout({ title, children, keywords, description }) {
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="keywords" content={keywords} />
         <meta name="description" content={description} />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Header />
-      <Search />  
+      <Search />
       <main className="container mx-auto my-7">{children}</main>
     </div>
   );
