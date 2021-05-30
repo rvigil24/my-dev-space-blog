19baed59b1ac5fdec0d93b870c77fdca0a3e0efe
Rubén Vigil <ruben.vigil24@gmail.com>
2021-05-29 23:35:53 -0600

    fixed slugs for posts

--- components/Pagination.js
+++ components/Pagination.js
@@ -10,7 +10,6 @@ export default function Pagination({ currentPage, numPages }) {
 
   return (
     <div className="bg-white px-4 py-3 flex items-center justify-center border-t border-gray-200 sm:px-6">
-
       {/* for mobile */}
 
       {/* previous button */}
@@ -26,9 +25,7 @@ export default function Pagination({ currentPage, numPages }) {
         {/* next button */}
         {currentPage < numPages && (
           <Link href={`/blog/page/${currentPage + 1}`}>
-            <a
-              className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-            >
+            <a className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
               Next
             </a>
           </Link>
@@ -36,7 +33,7 @@ export default function Pagination({ currentPage, numPages }) {
       </div>
 
       {/* large devices */}
-      <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+      <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between md:flex md:justify-end md:items-end">
         <div>
           <nav
             className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
